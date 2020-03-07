ca776a000666073f8b8889366590d8c1a8e86fce
Svetlana Guma <guma.svetlana@gmail.com>
2020-03-07 20:36:05 +0300

    init project

---
+++ src/main/java/com/lanagj/adviseme/AdvisemeApplication.java
@@ -0,0 +1,13 @@
+package com.lanagj.adviseme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanagj/adviseme/AdvisemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanagj.adviseme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
