0ec521f477efb475ab76deb8c9f6dcd2a92f9edc
Svetlana Guma <guma.svetlana@gmail.com>
2020-04-12 21:07:53 +0300

    fix tmdb beans

--- src/main/java/com/lanagj/adviseme/configuration/tmdb/TmdbConfiguration.java
+++ src/main/java/com/lanagj/adviseme/configuration/tmdb/TmdbConfiguration.java
@@ -8,25 +8,42 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class TmdbConfiguration {
 
     @Value("${tmdb.secret}")
     String tmdbSecret;
 
-    private final Tmdb tmdb;
+    private Tmdb tmdb;
 
-    public TmdbConfiguration() {
+    @PostConstruct
+    public void init() {
         tmdb = new Tmdb(this.tmdbSecret);
     }
 
     @Bean
     public MoviesService getMoviesService() {
+        if(tmdb.apiKey() == null) {
+            throw new IllegalArgumentException("TMDb API key is not set!");
+        }
         return tmdb.moviesService();
     }
 
     @Bean
     public DiscoverService getDiscoverService() {
+        if(tmdb.apiKey() == null) {
+            throw new IllegalArgumentException("TMDb API key is not set!");
+        }
         return tmdb.discoverService();
+    }
+
+    @Bean
+    public GenresService getGenresService() {
+        if(tmdb.apiKey() == null) {
+            throw new IllegalArgumentException("TMDb API key is not set!");
+        }
+        return tmdb.genreService();
     }
 }
